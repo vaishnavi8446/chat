fb3cd84c7242401de3c54079e240e46714e1cca2
vaishnavi8446 <vaishnaviambolkar8446@gmail.com>
2024-03-30 15:58:09 +0530

    document formatted

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
-const mongoose = require('mongoose');
-const Msg = require('./models/messages');
-const io = require('socket.io')(3000)
-const mongoDB = 'mongodb://localhost:27017/chat';
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    console.log('DB Connected')
-}).catch(err => console.log(err))
-io.on('connection', (socket) => {
-    Msg.find().then(result => {
-        socket.emit('output-messages', result)
-    })
-    console.log('User connected');
-    socket.emit('message', 'Hello world');
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
+const mongoose = require("mongoose");
+const Msg = require("./models/messages");
+const io = require("socket.io")(3000);
+
+const mongoDB = "mongodb://localhost:27017/chat";
+
+mongoose
+  .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => console.log(err));
+
+io.on("connection", (socket) => {
+  Msg.find().then((result) => {
+    socket.emit("output-messages", result);
+  });
+  console.log("User connected");
+
+  socket.emit("message", "Hello world");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  socket.on("chatmessage", (msg) => {
+    const message = new Msg({ msg });
+    message.save().then(() => {
+      io.emit("message", msg);
     });
-    socket.on('chatmessage', msg => {
-        const message = new Msg({ msg });
-        message.save().then(() => {
-            io.emit('message', msg)
-        })
-
-
-    })
+  });
 });
